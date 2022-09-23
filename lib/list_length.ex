9985dce10f07ee0543f36066ed0915470890cea7
Gustavo Hassen <gustavo.hassen91@gmail.com>
2022-09-22 22:57:06 -0300

    Solution of the problem

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum_elem(list, 0)
+
+  defp sum_elem([], acc), do: acc
+
+  defp sum_elem([_head | tail], acc) do
+    acc = acc + 1
+    sum_elem(tail, acc)
   end
 end
